13afc1f871c4871fc1925a57dcb027b9245361ca
RautRA <168980384+RautRA@users.noreply.github.com>
2024-08-25 20:30:20 +0530

    add code to add a new book to library

--- main.js
+++ main.js
@@ -20,6 +20,34 @@ const myLibrary = [
 ];
 
 const booksTableBody = document.querySelector(".books-table tbody");
+const addBookForm = document.querySelector(".add-book-form");
+
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(title, author, pages, readStatus) {
+    const newBook = new Book(title, author, pages, readStatus);
+    myLibrary.push(newBook);
+    return myLibrary;
+}
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = e.target.querySelector("#title").value;
+    const author = e.target.querySelector("#author").value;
+    const pages = e.target.querySelector("#pages").value;
+    let readStatus = e.target.querySelector("#read-status").value;
+    readStatus = (readStatus === "read") ? true : false;
+    
+    const newLibrary = addBookToLibrary(title, author, pages, readStatus);
+    renderLibrary(newLibrary);
+    addBookForm.reset();
+});
 
 function renderLibrary(library) {
     const books = library.map(book => {
@@ -28,7 +56,9 @@ function renderLibrary(library) {
                 <td>${book.pages}</td>
                 <td>${book.readStatus ? "Read" : "Not Read"}</td>`;
     });
-
+    
+    // resets the table body in case of rerender
+    booksTableBody.innerHTML = "";
     books.forEach(book => {
         booksTableBody.innerHTML += `<tr>${book}</tr>`;
     });
