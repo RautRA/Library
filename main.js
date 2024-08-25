a5e5a39aabe8c0235cadc9417cc1212f764a37d9
RautRA <168980384+RautRA@users.noreply.github.com>
2024-08-25 18:52:05 +0530

    add HTML, CSS & JS code to display books in library

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("Hello World!!!");
+const myLibrary = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: 366,
+        readStatus: false
+    },
+    {
+        title: "Harry Potter and the Sorcerer’s Stone",
+        author: "J.K. Rowling",
+        pages: 333,
+        readStatus: false
+    },
+    {
+        title: "A Game of Thrones",
+        author: "George R.R. Martin",
+        pages: 835,
+        readStatus: false
+    },
+];
+
+const booksTableBody = document.querySelector(".books-table tbody");
+
+function renderLibrary(library) {
+    const books = library.map(book => {
+        return `<td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td>${book.readStatus ? "Read" : "Not Read"}</td>`;
+    });
+
+    books.forEach(book => {
+        booksTableBody.innerHTML += `<tr>${book}</tr>`;
+    });
+}
+
+renderLibrary(myLibrary);
